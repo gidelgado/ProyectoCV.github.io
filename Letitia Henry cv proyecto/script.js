86a899d1a7252a2ec675c84ff58b1910198e4ffb
gidelgado <124105266+gidelgado@users.noreply.github.com>
2023-01-31 21:24:59 -0300

    Add files via upload

---
+++ Letitia Henry cv proyecto/script.js
@@ -0,0 +1,40 @@
+const verde = document.querySelector(".verde");
+
+verde.addEventListener("mouseenter", () => {
+verde.style.backgroundColor = "rgb(239, 245, 118)";
+})
+verde.addEventListener("mouseleave", ()=> {
+    verde.style.backgroundColor = "rgb(95, 246, 165)";
+})
+const verde2 = document.querySelector(".verde2");
+
+verde2.addEventListener("mouseenter", () => {
+verde2.style.backgroundColor = "rgb(239, 245, 118)";
+})
+verde2.addEventListener("mouseleave", ()=> {
+    verde2.style.backgroundColor = "rgb(95, 246, 165)";
+})
+const verde3 = document.querySelector(".verde3");
+
+verde3.addEventListener("mouseenter", () => {
+verde3.style.backgroundColor = "rgb(239, 245, 118)";
+})
+verde3.addEventListener("mouseleave", ()=> {
+    verde3.style.backgroundColor = "rgb(95, 246, 165)";
+})
+const verde4 = document.querySelector(".verde4");
+
+verde4.addEventListener("mouseenter", () => {
+verde4.style.backgroundColor = "rgb(239, 245, 118)";
+})
+verde4.addEventListener("mouseleave", ()=> {
+    verde4.style.backgroundColor = "rgb(95, 246, 165)";
+})
+const verde5 = document.querySelector(".verde5");
+
+verde5.addEventListener("mouseenter", () => {
+verde5.style.backgroundColor = "rgb(239, 245, 118)";
+})
+verde5.addEventListener("mouseleave", ()=> {
+    verde5.style.backgroundColor = "rgb(95, 246, 165)";
+})
